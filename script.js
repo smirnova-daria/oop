c7ecf26b043c3aba99aa4cb192c7ed41559cca58
Daria Smirnova <darya.smirn1@yandex.ru>
2022-04-16 16:09:50 +0300

    Выполнила задание урока №17

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict'
 
 class Animal {
+	static counter = +localStorage.getItem('counter') || 0
 	constructor(name, age, type, isHomeless, description) {
 		this._name = name
 		this._age = age
 		this._type = type
 		this._isHomeless = isHomeless
 		this._description = description
+		this._id = ++Animal.counter
 	}
 
 
@@ -26,7 +28,9 @@ class Animal {
 	get description() {
 		return this._description
 	}
-
+	get id() {
+		return this._id
+	}
 
 	//setters
 	set name(str) {
@@ -43,6 +47,17 @@ class Animal {
 	}
 	set description(str) {
 		this._description = str
+	}
+
+	deleteFromTable(id) {
+		animalObjects.forEach((obj, index) => {
+			if (obj.id == id) {
+				animalObjects.splice(animalObjects[index], 1)
+				animals.splice(animals[index], 1)
+				localStorage.setItem('animals', JSON.stringify(animals))
+				renderTable(animalObjects);
+			}
+		})
 	}
 
 }
@@ -170,13 +185,19 @@ class Cattle extends Animal {
 		this._isGivesMilk = boo
 	}
 }
-
-// const cat = new Cat()
-// cat['name'] = 'catty'
-
-// console.log(cat)
-
+const translates = {
+	cat: 'Коты',
+	dog: 'Собаки',
+	bird: 'Птицы',
+	rodent: 'Грызуны',
+	cattle: 'Крупный рогатый скот'
+}
 const animals = JSON.parse(localStorage.getItem('animals')) || []
+
+if (animals.length === 0) {
+	Animal.counter = 0
+	localStorage.removeItem('counter')
+}
 
 const form = document.querySelector('.form')
 const submit = document.getElementById('submit')
@@ -184,12 +205,16 @@ const isHomeless = document.getElementById('is-homeless')
 const animalName = document.getElementById('animal-name')
 const animalType = document.getElementById('animal-type')
 
+let deleteBtn = document.querySelectorAll('.delete-btn')
+let animalObjects = []
+
+
 const createAnimal = function (args) {
 	const params = [...args]
 	const inputValues = []
 	params.forEach(parametr => {
 		const value = {
-			id: parametr.name,
+			name: parametr.name,
 			val: parametr.value,
 			isChecked: parametr.checked
 		}
@@ -202,29 +227,35 @@ const createAnimal = function (args) {
 			newAnimal = new Cat()
 			setProperties(newAnimal, inputValues)
 			animals.push(newAnimal)
+			animalObjects.push(newAnimal)
 			break;
 		case 'dog':
 			newAnimal = new Dog()
 			setProperties(newAnimal, inputValues)
 			animals.push(newAnimal)
+			animalObjects.push(newAnimal)
 			break;
 		case 'bird':
 			newAnimal = new Bird()
 			setProperties(newAnimal, inputValues)
 			animals.push(newAnimal)
+			animalObjects.push(newAnimal)
 			break;
 		case 'rodent':
 			newAnimal = new Rodent()
 			setProperties(newAnimal, inputValues)
 			animals.push(newAnimal)
+			animalObjects.push(newAnimal)
 			break;
 		case 'cattle':
 			newAnimal = new Cattle()
 			setProperties(newAnimal, inputValues)
 			animals.push(newAnimal)
+			animalObjects.push(newAnimal)
 			break;
 	}
 	localStorage.setItem('animals', JSON.stringify(animals))
+	localStorage.setItem('counter', Animal.counter)
 }
 
 const setProperties = function (animal, props) {
@@ -232,24 +263,99 @@ const setProperties = function (animal, props) {
 		if (prop.val) {
 			if (index !== props.length - 1) {
 				if (index === 2) {
-					animal[prop.id] = prop.isChecked
+					animal[prop.name] = prop.isChecked
 				} else if (index === 8) {
 					if (animal.type === 'cattle') {
-						animal[prop.id] = prop.isChecked
+						animal[prop.name] = prop.isChecked
 					}
 				} else {
-					animal[prop.id] = prop.val
+					animal[prop.name] = prop.val
 				}
 			}
 		}
 	})
 }
 
-const addRow = function () {
-
+const setValuesForAnimal = function (animal, props) {
+	for (let key in props) {
+		animal[key] = props[key]
+	}
 }
 
-const renderTable = function () {
+const createAnimalObjects = function (animals) {
+	const animalObjs = []
+	let newAnimal
+	animals.forEach(animal => {
+		switch (animal._type) {
+			case 'cat':
+				newAnimal = new Cat()
+				setValuesForAnimal(newAnimal, animal)
+				animalObjs.push(newAnimal)
+				break;
+			case 'dog':
+				newAnimal = new Dog()
+				setValuesForAnimal(newAnimal, animal)
+				animalObjs.push(newAnimal)
+				break;
+			case 'bird':
+				newAnimal = new Bird()
+				setValuesForAnimal(newAnimal, animal)
+				animalObjs.push(newAnimal)
+				break;
+			case 'rodent':
+				newAnimal = new Rodent()
+				setValuesForAnimal(newAnimal, animal)
+				animalObjs.push(newAnimal)
+				break;
+			case 'cattle':
+				newAnimal = new Cattle()
+				setValuesForAnimal(newAnimal, animal)
+				animalObjs.push(newAnimal)
+				break;
+		}
+	})
+	return animalObjs;
+}
+
+const renderTable = function (arr) {
+	const table = document.querySelector('table')
+	table.querySelectorAll('tr:not(:first-child)').forEach(tr => {
+		tr.remove()
+	})
+	arr.forEach(animal => {
+		const tr = document.createElement('tr')
+		tr.dataset.index = animal._id
+		tr.innerHTML = `<td>${translates[animal._type]}</td>
+					<td>${animal._name.toUpperCase()[0] + animal._name.toLowerCase().slice(1) || 'Безымянный'}</td>
+					<td>${animal._isHomeless ? '&#10004;': '&#10008;'}</td>
+					<td>${animal._age}</td>
+					<td>${animal._breed || 'Не предусмотрено'}</td>
+					<td>${animal._size || 'Не предусмотрено'}</td>
+					<td>${animal._kind || 'Не предусмотрено'}</td>
+					<td>${animal._color || 'Не предусмотрено'}</td>
+					<td>${animal._isGivesMilk ? '&#10004;' : '&#10008;'}</td>
+					<td>${animal._description}</td>
+					<td>
+						<button class="delete-btn" data-index-btn="${animal._id}">Удалить</button>
+					</td>`
+		table.append(tr)
+	})
+	deleteBtn = table.querySelectorAll('.delete-btn')
+	deleteBtn.forEach(btn => {
+		const id = btn.dataset.indexBtn
+		let animalForDelete
+		animalObjects.forEach(obj => {
+			if (obj.id == id) {
+				animalForDelete = obj
+			}
+		})
+		btn.addEventListener('click', () => {
+			animalForDelete.deleteFromTable(id)
+		})
+	})
+}
+
+const createTable = function () {
 	const contentTable = document.querySelector('.content__table')
 	const table = document.createElement('table')
 	table.classList.add('table')
@@ -269,23 +375,6 @@ const renderTable = function () {
 					</th>`
 	table.append(tr)
 
-	animals.forEach(animal => {
-		const tr = document.createElement('tr')
-		tr.innerHTML = `<td>${animal._type}</td>
-					<td>${animal._name || 'Безымянный'}</td>
-					<td>${animal._isHomeless ? 'ДА': 'НЕТ'}</td>
-					<td>${animal._age}</td>
-					<td>${animal._breed || '-'}</td>
-					<td>${animal._size || '-'}</td>
-					<td>${animal._kind || '-'}</td>
-					<td>${animal._color || '-'}</td>
-					<td>${animal._isGivesMilk ? 'ДА': 'НЕТ'}</td>
-					<td>${animal._description}</td>
-					<td>
-						<button class="delete-btn"> Удалить </button>
-					</td>`
-		table.append(tr)
-	})
 	contentTable.append(table)
 }
 
@@ -293,7 +382,7 @@ form.addEventListener('submit', (event) => {
 	event.preventDefault()
 
 	createAnimal(Array.from(event.target));
-	// addRow();
+	renderTable(animals)
 
 	//очистка инпутов
 	const inputs = form.querySelectorAll('select, input[type=text], input[type=number], textarea')
@@ -306,7 +395,6 @@ form.addEventListener('submit', (event) => {
 		check.checked = false
 	})
 
-	console.log(animals)
 })
 
 isHomeless.addEventListener('change', () => {
@@ -366,4 +454,7 @@ animalType.addEventListener('change', (event) => {
 	}
 })
 
-renderTable()
+
+createTable()
+animalObjects = createAnimalObjects(animals)
+renderTable(animalObjects)
